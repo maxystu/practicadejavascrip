88fc8e937224f0960d15f544a8cb4ba30ed77e41
Maxi Rios <maxi.rios704@gmail.com>
2024-07-16 21:32:18 -0300

    practica de variables

---
+++ js/01-variables.js
@@ -0,0 +1,21 @@
+//esto es un comentario de una linea
+
+/* esto es un comentario 
+de
+multiples lineas */
+
+
+//declarar o crear una variable(var- let- const)
+
+let edad = 30;
+let encendido = false ;
+
+//mostrar el contenido de una variable
+console.log(edad)
+document.write("<p class='textoresaltado'>hola mundo</p>")
+document.write("edad: " + edad + "<br>")
+
+//modificar el contenido de una variable
+
+edad = 30 + 3;
+document.write("edad: " + edad)
